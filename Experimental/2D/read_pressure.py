057b78977f5287408f161342f2dfb6ec352376a3
djaalanG <djaalangort@gmail.com>
2024-12-19 11:59:53 +0100

    Update my file

--- Experimental/2D/read_pressure.py
+++ Experimental/2D/read_pressure.py
@@ -3,9 +3,22 @@ import numpy as np
 import pandas as pd
 import itertools as it
 import matplotlib.pyplot as plt
+import math
+import sys
+sys.path.insert(1, 'Experimental')
+
+
+
+
+
+
+import Ambient_Results.ambient_results_rho as ar
 
 #change depending on which row
 row_number = 12
+
+density=ar.get_ambient_results_new(row_number)[0]
+print(density)
 
 
 # Read the data from the file, skipping the first row
@@ -51,15 +64,68 @@ for i in range(105,117,1):
     p_stat.append(stat)
 
 
+p_dynamic=[]
+
+for i in range(47):
+    if i <=7:
+        dynamic= p_tot[i]-p_stat[0]
+        p_dynamic.append(dynamic)
+    elif i in range(8,10,1):
+        dynamic= p_tot[i]-p_stat[1]
+        p_dynamic.append(dynamic)
+    elif i in range(10,13,1):
+        dynamic= p_tot[i]-p_stat[2]
+        p_dynamic.append(dynamic)
+    elif i in range(13,17,1):
+        dynamic= p_tot[i]-p_stat[3]
+        p_dynamic.append(dynamic)
+    elif i in range(17,21,1):
+        dynamic= p_tot[i]-p_stat[4]
+        p_dynamic.append(dynamic)
+    elif i in range(21,25,1):
+        dynamic= p_tot[i]-p_stat[5]
+        p_dynamic.append(dynamic)
+    elif i in range(25,29,1):
+        dynamic= p_tot[i]-p_stat[6]
+        p_dynamic.append(dynamic)
+    elif i in range(29,33,1):
+        dynamic= p_tot[i]-p_stat[7]
+        p_dynamic.append(dynamic)
+    elif i in range(33,37,1):
+        dynamic= p_tot[i]-p_stat[8]
+        p_dynamic.append(dynamic)
+    elif i in range(37,40,1):
+        dynamic= p_tot[i]-p_stat[9]
+        p_dynamic.append(dynamic)
+    elif i in range(40,42,1):
+        dynamic= p_tot[i]-p_stat[10]
+        p_dynamic.append(dynamic)
+    else:
+        dynamic= p_tot[i]-p_stat[11]
+        p_dynamic.append(dynamic)
+
+velocity=[]
+for i in range(47):
+    speed= math.sqrt(p_dynamic[i]/density)
+
+
+
+
+
+
+
 # #plot the thingies
 plt.plot(x_dist_tot, p_tot)
 plt.plot(x_dist_stat,p_stat)
+plt.plot(x_dist_tot,p_dynamic)
 
 # Add a title, axis labels, and a legend
 plt.title('One period of the sine and cosine functions')
 plt.xlabel('x [rad]')
 plt.ylabel('y')
-plt.legend(('sin(x)', 'cos(x)'))
+plt.legend(('sin(x)', 'cos(x)',"cool line"))
 
 # Don't forget to call show()!
 plt.show()
+
+print(len(x_dist_tot))
